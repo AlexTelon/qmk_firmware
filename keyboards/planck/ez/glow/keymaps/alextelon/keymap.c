de735e12036c49fa5e75f1ecb9ea302b15b2bc1e
Alex Telon <alex.telon@gmail.com>
2022-11-24 23:37:36 +0100

    Going to swe layer now changes windows lang too
    _LAYER6 is my swedish layer with åäö on and nothing else special.
    Need to change windows to swedish locale however and now this is done
    when changing to that layer and back.
    
    It can get out of sync if you run win+space manually. But I prefer that
    to setting custom keybinds to switch to a specific language in windows.
    Its just a win+space toggle away in that case so thats fine.
    
    The main reason for this layer is that I want to use backspace in the
    top right corner for normal english use. But when writing swedish i need
    that key for å. That is a seperate issue that I want to fix as well,
    using backspace when writing swedish..

--- keyboards/planck/ez/glow/keymaps/alextelon/keymap.c
+++ keyboards/planck/ez/glow/keymaps/alextelon/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_Q,           KC_W,           LT(2,KC_E),     MT(MOD_LGUI, KC_R),KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,      
     MT(MOD_LCTL, KC_TAB),KC_A,           MT(MOD_LSFT, KC_S),MT(MOD_LALT, KC_D),MT(MOD_LCTL, KC_F),MT(MOD_LGUI, KC_G),MT(MOD_LGUI, KC_H),MT(MOD_LCTL, KC_J),MT(MOD_LALT, KC_K),MT(MOD_LSFT, KC_L),KC_SCOLON,      KC_QUOTE,       
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           LT(_LOWER, KC_V),KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       TG(2),          
-    KC_INSERT,      TD(DANCE_0),    TT(6),          RAISE,          MO(4),          LT(_LOWER, KC_SPACE),KC_NO,          LT(_LOWER, KC_ENTER),MO(4),          KC_LPRN,        KC_RPRN,        TG(4)
+    KC_INSERT,      TD(DANCE_0),    TG(6),          RAISE,          MO(4),          LT(_LOWER, KC_SPACE),KC_NO,          LT(_LOWER, KC_ENTER),MO(4),          KC_LPRN,        KC_RPRN,        TG(4)
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
@@ -198,6 +198,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         rgblight_mode(1);
       }
       return false;
+    case TG(_LAYER6):
+        if (record->event.pressed) {
+            // When going to the swedish layer we swap language mode
+            // (default keybinding in windows is Win+spacebar
+            register_mods(MOD_LGUI);
+            tap_code(KC_SPACE);
+            unregister_mods(MOD_LGUI);
+        }
+
+        break;
   }
   return true;
 }
